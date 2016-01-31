c498438f2d91dbb3f730f34da2d38dc2eeef795a
lgrignon <louis.grignon@gmail.com>
2016-01-31 19:37:22 +0100

    added ionic

--- src/main/java/org/jsweet/examples/cordova/App.java
+++ src/main/java/org/jsweet/examples/cordova/App.java
@@ -1,51 +1,253 @@
 package org.jsweet.examples.cordova;
-import static jsweet.util.StringTypes.deviceready;
+
+import static def.angularjs.Globals.angular;
 import static jsweet.dom.Globals.console;
 import static jsweet.dom.Globals.document;
+import static jsweet.util.Globals.array;
+import static jsweet.util.Globals.function;
+import static jsweet.util.Globals.union;
+import static jsweet.util.StringTypes.deviceready;
 
-import jsweet.dom.Element;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import def.angular_ui_router.ng.ui.IState;
+import def.angular_ui_router.ng.ui.IStateProvider;
+import def.angular_ui_router.ng.ui.IUrlRouterProvider;
+import def.angularjs.ng.IModule;
+import def.angularjs.ng.IScope;
+import def.ionic.ionic.sidemenu.IonicSideMenuDelegate;
 import jsweet.dom.Event;
+import jsweet.lang.Array;
+import jsweet.lang.Interface;
+import jsweet.lang.Object;
 
-public class App {
-	
-	public static App instance = new App();
-	
-	App() {}
-	
-	 // Application Constructor
-    public void initialize() {
-        this.bindEvents();
-    }
-    
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // "load", "deviceready", "offline", and "online".
-    public void bindEvents() {
-        document.addEventListener(deviceready, this::onDeviceReady, false);
-    }
-    // deviceready Event Handler
-    //
-    // The scope of "this" is the event. In order to call the "receivedEvent"
-    // function, we must explicitly call "app.receivedEvent(...);"
-    public Object onDeviceReady(Object e) {
-        receivedEvent("deviceready");
-        return null;
-    }
-    // Update DOM on a Received Event
-    public void receivedEvent(String id) {
-        Element parentElement = document.getElementById(id);
-        Element listeningElement = parentElement.querySelector(".listening");
-        Element receivedElement = parentElement.querySelector(".received");
+class Playlist {
 
-        listeningElement.setAttribute("style", "display:none;");
-        receivedElement.setAttribute("style", "display:block;");
+	String title;
+	int id;
 
-        console.log("Received Event: " + id);
-    }
-    
-    public static void main(String[] args) {
-    	App.instance.initialize();
+	Playlist(String title, int id) {
+		super();
+		this.title = title;
+		this.id = id;
 	}
 }
 
+class PlaylistsService {
+	public Playlist[] getPlaylists() {
+		return new Playlist[] { new Playlist("Reggae", 1), new Playlist("Chill", 2), new Playlist("Dubstep", 3),
+				new Playlist("Indie", 4), new Playlist("Rap", 5), new Playlist("Cowbell", 6) };
+	}
+
+	public Playlist[] searchPlaylists(String search) {
+		return array(getPlaylists()).filter((p, __, ___) -> {
+			return p.title.indexOf(search) != -1;
+		});
+	}
+}
+
+class BrowseSearch {
+	String query;
+}
+
+class BrowseViewModel {
+	BrowseSearch search;
+
+	Array<Playlist> playlistsResult;
+
+	Runnable onSearch;
+}
+
+class BrowseController {
+
+	private BrowseViewModel $scope;
+	private PlaylistsService playlistsService;
+
+	BrowseController(BrowseViewModel $scope, IonicSideMenuDelegate $ionicSideMenuDelegate,
+			PlaylistsService playlistsService) {
+		this.$scope = $scope;
+		this.playlistsService = playlistsService;
+
+		this.$scope.search = new BrowseSearch();
+		this.$scope.onSearch = this::onSearch;
+		this.$scope.playlistsResult = new Array<>();
+	}
+
+	private void onSearch() {
+		console.log("search=" + $scope.search.query);
+		Playlist[] playlists = playlistsService.searchPlaylists($scope.search.query);
+
+		console.log("playlists", playlists);
+		$scope.playlistsResult.splice(0, $scope.playlistsResult.length);
+		for (int i = 0; i < playlists.length; i++) {
+			$scope.playlistsResult.push(playlists[i]);
+		}
+	}
+}
+
+@Interface
+abstract class PlaylistsViewModel extends IScope {
+	Playlist[] playlists;
+}
+
+class PlaylistsController {
+	PlaylistsViewModel $scope;
+
+	PlaylistsController(PlaylistsViewModel $scope) {
+		this.$scope = $scope;
+
+		$scope.playlists = new Playlist[] { new Playlist("Reggae", 1), new Playlist("Chill", 2),
+				new Playlist("Dubstep", 3), new Playlist("Indie", 4), new Playlist("Rap", 5),
+				new Playlist("Cowbell", 6) };
+	}
+}
+
+class Pizza extends jsweet.lang.Object {
+	boolean pepperoni = true;
+	boolean sausage = false;
+	boolean anchovies = true;
+	boolean jalapenos = false;
+}
+
+@Interface
+abstract class PizzaViewModel extends IScope {
+	Pizza pizza;
+	Supplier<String> toppings;
+}
+
+class PizzaController {
+	PizzaViewModel $scope;
+
+	PizzaController(PizzaViewModel $scope) {
+		this.$scope = $scope;
+
+		$scope.pizza = new Pizza();
+		$scope.toppings = this::getToppings;
+	}
+
+	String getToppings() {
+		String[] toppings = array(Object.keys($scope.pizza)).filter((flavor, __, ___) -> {
+			return (Boolean) $scope.pizza.$get(flavor);
+		});
+
+		if (toppings.length > 1) {
+			toppings[toppings.length - 1] = "and " + toppings[toppings.length - 1];
+		}
+		if (toppings.length > 2) {
+			return array(toppings).join(", ");
+		} else if (toppings.length > 0) {
+			return array(toppings).join(" ");
+		} else {
+			return "nothing";
+		}
+	}
+}
+
+public class App {
+
+	public static App instance = new App();
+
+	public void initialize() {
+		this.bindEvents();
+	}
+
+	public void bindEvents() {
+		Function<Event, Object> handler = this::onDeviceReady;
+		document.addEventListener(deviceready, handler, false);
+	}
+
+	public Object onDeviceReady(Event e) {
+		console.log("device ready!");
+		return null;
+	}
+
+	// public void takePicture() {
+	// navigator.camera.getPicture(function(imageURI) {
+	//
+	// // imageURI is the URL of the image that we can use for
+	// // an <img> element or backgroundImage.
+	//
+	// }, function(err) {
+	//
+	// // Ruh-roh, something bad happened
+	//
+	// }, cameraOptions);
+	// }
+
+	public static void main(String[] args) {
+		App.instance.initialize();
+
+		IModule app = angular.module("starter", new String[] { "ionic" });
+
+		app.config(function((IStateProvider $stateProvider, IUrlRouterProvider $urlRouterProvider) -> {
+
+			$stateProvider.state("app", new IState() {
+				{
+					url = union("/app");
+					Abstract = true;
+					templateUrl = union("templates/menu.html");
+				}
+			}).state("app.browse", new IState() {
+				{
+					url = union("/browse");
+					views = new Views() {
+						{
+							$set("menuContent", new IState() {
+								{
+									templateUrl = union("templates/browse.html");
+									$set("controller", "BrowseController");
+								}
+							});
+						}
+					};
+				}
+			}).state("app.playlists", new IState() {
+				{
+					url = union("/playlists");
+					views = new Views() {
+						{
+							$set("menuContent", new IState() {
+								{
+									templateUrl = union("templates/playlists.html");
+									$set("controller", "PlaylistsController");
+								}
+							});
+						}
+					};
+				}
+			}).state("app.pizza", new IState() {
+				{
+					url = union("/pizza");
+					views = new Views() {
+						{
+							$set("menuContent", new IState() {
+								{
+									templateUrl = union("templates/pizza.html");
+									$set("controller", "PizzaController");
+								}
+							});
+						}
+					};
+				}
+			});
+			$urlRouterProvider.otherwise("/app/browse");
+		}));
+
+		jsweet.lang.Function controller = function(PlaylistsController.class);
+		console.log("PlaylistsController: ", controller);
+		app.controller("PlaylistsController", controller);
+
+		controller = function(BrowseController.class);
+		console.log("BrowseController: ", controller);
+		app.controller("BrowseController", controller);
+
+		controller = function(PizzaController.class);
+		console.log("PizzaController: ", controller);
+		app.controller("PizzaController", controller);
+
+		jsweet.lang.Function playlistsService = function(PlaylistsService.class);
+		console.log("playlistsService: ", controller);
+		app.service("playlistsService", playlistsService);
+	}
+}
--- www/js/app/org/jsweet/examples/cordova/App.js
+++ www/js/app/org/jsweet/examples/cordova/App.js
@@ -1,4 +1,4 @@
-"Generated from Java with JSweet 1.0.0-SNAPSHOT - http://www.jsweet.org";
+"Generated from Java with JSweet 1.0.0-RC1 - http://www.jsweet.org";
 var org;
 (function (org) {
     var jsweet;
@@ -7,6 +7,111 @@ var org;
         (function (examples) {
             var cordova;
             (function (cordova) {
+                var Playlist = (function () {
+                    function Playlist(title, id) {
+                        ;
+                        this.title = title;
+                        this.id = id;
+                    }
+                    return Playlist;
+                })();
+                cordova.Playlist = Playlist;
+                var PlaylistsService = (function () {
+                    function PlaylistsService() {
+                    }
+                    PlaylistsService.prototype.getPlaylists = function () {
+                        return [new Playlist("Reggae", 1), new Playlist("Chill", 2), new Playlist("Dubstep", 3),
+                            new Playlist("Indie", 4), new Playlist("Rap", 5), new Playlist("Cowbell", 6)];
+                    };
+                    PlaylistsService.prototype.searchPlaylists = function (search) {
+                        return (this.getPlaylists()).filter(function (p, __, ___) {
+                            return p.title.indexOf(search) != -1;
+                        });
+                    };
+                    return PlaylistsService;
+                })();
+                cordova.PlaylistsService = PlaylistsService;
+                var BrowseSearch = (function () {
+                    function BrowseSearch() {
+                    }
+                    return BrowseSearch;
+                })();
+                cordova.BrowseSearch = BrowseSearch;
+                var BrowseViewModel = (function () {
+                    function BrowseViewModel() {
+                    }
+                    return BrowseViewModel;
+                })();
+                cordova.BrowseViewModel = BrowseViewModel;
+                var BrowseController = (function () {
+                    function BrowseController($scope, $ionicSideMenuDelegate, playlistsService) {
+                        var _this = this;
+                        this.$scope = $scope;
+                        this.playlistsService = playlistsService;
+                        this.$scope.search = new BrowseSearch();
+                        this.$scope.onSearch = function () { return _this.onSearch(); };
+                        this.$scope.playlistsResult = new Array();
+                    }
+                    BrowseController.prototype.onSearch = function () {
+                        console.log("search=" + this.$scope.search.query);
+                        var playlists = this.playlistsService.searchPlaylists(this.$scope.search.query);
+                        console.log("playlists", playlists);
+                        this.$scope.playlistsResult.splice(0, this.$scope.playlistsResult.length);
+                        for (var i = 0; i < playlists.length; i++) {
+                            this.$scope.playlistsResult.push(playlists[i]);
+                        }
+                    };
+                    return BrowseController;
+                })();
+                cordova.BrowseController = BrowseController;
+                var PlaylistsController = (function () {
+                    function PlaylistsController($scope) {
+                        this.$scope = $scope;
+                        $scope.playlists = [new Playlist("Reggae", 1), new Playlist("Chill", 2),
+                            new Playlist("Dubstep", 3), new Playlist("Indie", 4), new Playlist("Rap", 5),
+                            new Playlist("Cowbell", 6)];
+                    }
+                    return PlaylistsController;
+                })();
+                cordova.PlaylistsController = PlaylistsController;
+                var Pizza = (function () {
+                    function Pizza() {
+                        this.pepperoni = true;
+                        this.sausage = false;
+                        this.anchovies = true;
+                        this.jalapenos = false;
+                    }
+                    return Pizza;
+                })();
+                cordova.Pizza = Pizza;
+                var PizzaController = (function () {
+                    function PizzaController($scope) {
+                        var _this = this;
+                        this.$scope = $scope;
+                        $scope.pizza = new Pizza();
+                        $scope.toppings = function () { return _this.getToppings(); };
+                    }
+                    PizzaController.prototype.getToppings = function () {
+                        var _this = this;
+                        var toppings = (Object.keys(this.$scope.pizza)).filter(function (flavor, __, ___) {
+                            return _this.$scope.pizza[flavor];
+                        });
+                        if ((toppings.length > 1)) {
+                            toppings[toppings.length - 1] = "and " + toppings[toppings.length - 1];
+                        }
+                        if ((toppings.length > 2)) {
+                            return (toppings).join(", ");
+                        }
+                        else if ((toppings.length > 0)) {
+                            return (toppings).join(" ");
+                        }
+                        else {
+                            return "nothing";
+                        }
+                    };
+                    return PizzaController;
+                })();
+                cordova.PizzaController = PizzaController;
                 var App = (function () {
                     function App() {
                     }
@@ -15,22 +120,38 @@ var org;
                     };
                     App.prototype.bindEvents = function () {
                         var _this = this;
-                        document.addEventListener("deviceready", function (e) { return _this.onDeviceReady(e); }, false);
+                        var handler = function (e) { return _this.onDeviceReady(e); };
+                        document.addEventListener("deviceready", handler, false);
                     };
                     App.prototype.onDeviceReady = function (e) {
-                        this.receivedEvent("deviceready");
+                        console.log("device ready!");
                         return null;
-                    };
-                    App.prototype.receivedEvent = function (id) {
-                        var parentElement = document.getElementById(id);
-                        var listeningElement = parentElement.querySelector(".listening");
-                        var receivedElement = parentElement.querySelector(".received");
-                        listeningElement.setAttribute("style", "display:none;");
-                        receivedElement.setAttribute("style", "display:block;");
-                        console.log("Received Event: " + id);
                     };
                     App.main = function (args) {
                         App.instance.initialize();
+                        var app = angular.module("starter", ["ionic"]);
+                        app.config((function ($stateProvider, $urlRouterProvider) {
+                            $stateProvider.state("app", { url: "/app", Abstract: true, templateUrl: "templates/menu.html" }).state("app.browse", { url: "/browse", views: {
+                                    "menuContent": { templateUrl: "templates/browse.html",
+                                        "controller": "BrowseController" } } }).state("app.playlists", { url: "/playlists", views: {
+                                    "menuContent": { templateUrl: "templates/playlists.html",
+                                        "controller": "PlaylistsController" } } }).state("app.pizza", { url: "/pizza", views: {
+                                    "menuContent": { templateUrl: "templates/pizza.html",
+                                        "controller": "PizzaController" } } });
+                            $urlRouterProvider.otherwise("/app/browse");
+                        }));
+                        var controller = PlaylistsController;
+                        console.log("PlaylistsController: ", controller);
+                        app.controller("PlaylistsController", controller);
+                        controller = BrowseController;
+                        console.log("BrowseController: ", controller);
+                        app.controller("BrowseController", controller);
+                        controller = PizzaController;
+                        console.log("PizzaController: ", controller);
+                        app.controller("PizzaController", controller);
+                        var playlistsService = PlaylistsService;
+                        console.log("playlistsService: ", controller);
+                        app.service("playlistsService", playlistsService);
                     };
                     App.instance = new App();
                     return App;
